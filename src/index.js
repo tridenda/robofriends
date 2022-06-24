a567c68b810445d62dd6e17971b3e43ff99c1c5d
Tri Denda <tridenda.nk@gmail.com>
2022-06-24 12:30:18 +0700

    update things

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "tachyons";
+import Card from "./Card";
+import { robots } from "./robots";
+
+ReactDOM.render(
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
